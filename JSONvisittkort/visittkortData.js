744017741a83c5ad977139098763a9cf1ebac2b3
e4vari <113419440+e4vari@users.noreply.github.com>
2023-05-25 18:26:15 +0200

    Add files via upload

---
+++ JSONvisittkort/visittkortData.js
@@ -0,0 +1,6 @@
+let visittKort = {
+    "personArray": [{ "navn": "elahe ", "tlf": "42484811","stilling":"elev", "kjoonn":"jente", "bursdag":"02.november 2005", "forekort":"ikke gammel nok for førekort!", "profilBilde":"wonyoung.jpg"},
+    { "navn": "evie", "tlf": "1112131415","stilling":"designer", "kjoonn":"jente", "bursdag":"21.november 1989", "profilBilde":"nai.jpg"},
+    { "navn": "alia", "tlf": "109876543","stilling":"doktor", "kjoonn":"dame", "bursdag":"25.august 2000", "forekort":"har førekort!", "profilBilde":"belle.jpg"}]
+}
+
